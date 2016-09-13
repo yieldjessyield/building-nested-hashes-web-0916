9216e8698f22a8a132d45ee92c7f7d8a3ae2ff4b
Jessica Harrelson <jess.harrelson0707@gmail.com>
2016-09-13 16:52:41 -0400

    added a note

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -29,3 +29,5 @@ epic_tragedy[:capulet][:heroine][:status] = "dead"
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+
+#This is a note :)
